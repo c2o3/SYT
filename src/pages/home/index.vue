4298efbf0dccd34b49b2ce1e142765e407b18abe
c2o3 <1263539715@qq.com>
2023-11-21 23:19:12 +0800

    子组件自定义事件还是挺重要的

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -8,11 +8,11 @@
     <el-row gutter="20">
       <el-col :span="20">
         <!-- 等级子组件 -->
-        <Level />
+        <Level @getLevel="getLevel"/>
         <!-- 地区 -->
-        <Region />
+        <Region @getRegion="getRegion"/>
         <!-- 展示医院的结构 -->
-        <div class="hospital">
+        <div class="hospital" v-if="hasHospitalArr.length>0">
           <Card
             class="item"
             v-for="(item, index) in hasHospitalArr"
@@ -20,6 +20,7 @@
             :hospitalInfo="item"
           ></Card>
         </div>
+        <el-empty v-else description="暂无数据" />
         <!-- 分页器 -->
         <el-pagination
           v-model:current-page="pageNo"
@@ -46,14 +47,20 @@ import Level from "./level/index.vue";
 import Region from "./region/index.vue";
 import Card from "./card/index.vue";
 
+import type { Content,HospitalResponseData } from '@/api/home/type'
+
 //分页器页码
 let pageNo = ref<number>(1);
 //一页展示几条数据
 let pageSize = ref<number>(10);
 //存储已有的医院的数据
-let hasHospitalArr = ref([]);
+let hasHospitalArr = ref<Content>([]);
 //存储医院的总个数
-let total = ref(0);
+let total = ref<number>(0);
+//存储医院的等级响应数据
+let hostype = ref<string>('')
+//存储选择的医院的地区
+let districtCode = ref<string>('')
 //组件挂载完毕，发一次请求
 onMounted(() => {
   getHospitalInfo();
@@ -61,7 +68,7 @@ onMounted(() => {
 //获取已有的医院的数据
 const getHospitalInfo = async () => {
   //获取医院的数据：默认获取第一页、一页十个医院的数据
-  let result: any = await reqHospital(pageNo.value, pageSize.value);
+  let result: HospitalResponseData = await reqHospital(pageNo.value, pageSize.value,hostype.value,districtCode.value);
   if (result.code == 200) {
     //存储已有的医院的数据
     hasHospitalArr.value = result.data.content;
@@ -75,6 +82,19 @@ const currentChange = () => {
 }
 //分页器下拉菜单发生变化时触发
 const sizeChange = () => {
+  getHospitalInfo()
+}
+//子组件自定义事件：获取儿子给父组件传递过来的等级的参数
+const getLevel = (level:string) => {
+  //收集参数：等级参数
+  hostype.value = level
+  //再次发请求
+  getHospitalInfo()
+}
+//子组件自定义事件：获取子组件传递过来的地区参数
+const getRegion = (region:string) => {
+  //存储地区参数
+  districtCode.value = region
   getHospitalInfo()
 }
 </script>
--- src/pages/home/region/index.vue
+++ src/pages/home/region/index.vue
@@ -3,21 +3,52 @@
     <div class="content">
       <div class="left">地区：</div>
       <ul>
-        <li class="active">全部</li>
-        <li>昌平区</li>
-        <li>昌平区</li>
-        <li>昌平区</li>
-        <li>昌平区</li>
-        <li>昌平区</li>
-        <li>昌平区</li>
-        <li>昌平区</li>
-        <li>昌平区</li>
-        <li v-for="item in 10" :key="item">昌平区</li>
+        <li :class="{active:RegionFlag==''}" @click="changeRegion('')">全部</li>
+        <li v-for="item in RegionArr" :key="item.value" :class="{active:RegionFlag== item.value}" @click="changeRegion(item.value)">{{ item.name }}</li>
       </ul>
     </div>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reqHospitalLevelAndRegion } from "@/api/home";
+import { onMounted, ref } from "vue";
+import type {
+  HospitalLevelAndRegionResponseData,
+  HospitalLevelAndRegionArr,
+} from "@/api/home/type";
+//存储地区数据
+let RegionArr = ref<HospitalLevelAndRegionArr>([])
+//地区高亮响应式数据
+let RegionFlag = ref<string>('')
+//地区组件挂载完毕获取地区数据
+onMounted(() => {
+  getRegion();
+});
+const getRegion = async () => {
+  let result: HospitalLevelAndRegionResponseData =
+    await reqHospitalLevelAndRegion("Beijin");
+    console.log(result)
+  //存储全部地区的数据
+  if(result.code == 200){
+    RegionArr.value = result.data
+  }
+};
+
+//点击不同区域按钮的高亮
+const changeRegion = (region:string) => {
+  RegionFlag.value = region
+  //给父组件传递区域的参数
+  $emit('getRegion', region)
+}
+
+let $emit = defineEmits(['getRegion'])
+</script>
+
+<script lang="ts">
+export default{
+  name: 'Region'
+}
+</script>
 <style scoped lang="scss">
 .region {
   color: #7f7f7f;
@@ -32,14 +63,14 @@
       display: flex;
       flex-wrap: wrap;
       margin-left: -10px;
-      li{
+      li {
         margin-right: 5px;
         margin-bottom: 10px;
-        &.active{
+        &.active {
           color: aqua;
         }
       }
-      li:hover{
+      li:hover {
         color: aqua;
         cursor: pointer;
       }
